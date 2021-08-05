322fdf4b57b1aff4326412b5f9620d3ac6bdb424
Levi V <levi@redhat.com>
2021-08-05 20:49:32 +0200

    fixed bug in yml check that failed the script if variant values were missing

--- PantheonCMD/pcyamlcheck.py
+++ PantheonCMD/pcyamlcheck.py
@@ -2,7 +2,6 @@
 
 import os
 import yaml
-from pcutil import PantheonRepo
 
 
 class Printing():
@@ -27,7 +26,7 @@ class Printing():
                 print('\t' + separator.join(item))
 
 
-def empty_key_check(report, yaml_file, keys):
+def get_empty_values(report, yaml_file, keys):
     empty_keys = []
 
     for key in keys:
@@ -37,37 +36,55 @@ def empty_key_check(report, yaml_file, keys):
         report.create_report('values are empty', sorted(empty_keys, key=str.lower))
 
 
-def path_to_images_dir_check(report, yaml_file):
-    false_dir = []
+def get_missing_variant_keys(report, yaml_file, required_values):
+    missing_value = []
 
-    for item in (yaml_file['resources']):
-        path_to_images_dir = os.path.split(item)[0]
-        if not os.path.exists(path_to_images_dir):
-            false_dir.append(path_to_images_dir)
-    if false_dir:
-        report.create_report('files or directories do not exist in your repository', sorted(false_dir, key=str.lower))
+    if yaml_file['variants'] is None:
+        report.create_report('values are missing under "variants"', sorted(required_values, key=str.lower))
+        return
 
+    for item in required_values:
+        for variant_values in yaml_file['variants']:
+            if item not in variant_values.keys():
+                missing_value.append(item)
 
-def variant_values_check(report, yaml_file, required_values):
+    if missing_value:
+        report.create_report('keys are missing under "variants"', sorted(missing_value, key=str.lower))
+        return
+
     varriant_values_none = []
     cannonical_is_not_true = []
     path_does_not_exist = []
+    missing_value = []
 
     for variant_values in yaml_file['variants']:
         for item in required_values:
             if variant_values[item] == None:
                 varriant_values_none.append(item)
-                report.create_report('values are empty', sorted(varriant_values_none, key=str.lower))
+        if varriant_values_none:
+            report.create_report('keys are empty', sorted(varriant_values_none, key=str.lower))
 
         if variant_values['canonical'] != None:
             if variant_values['canonical'] != True:
                 cannonical_is_not_true.append('cannonical')
-                report.create_report('value is not set to True', sorted(cannonical_is_not_true, key=str.lower))
+        if cannonical_is_not_true:
+            report.create_report('key is not set to True', sorted(cannonical_is_not_true, key=str.lower))
 
         if variant_values['path'] != None:
             if not os.path.exists(variant_values['path']):
                 path_does_not_exist.append(variant_values['path'])
-                report.create_report('files or directories do not exist in your repository', path_does_not_exist)
+        if path_does_not_exist:
+            report.create_report('files or directories do not exist in your repository', path_does_not_exist)
+
+    false_dir = []
+
+    if yaml_file['resources'] != None:
+        for item in (yaml_file['resources']):
+            path_to_images_dir = os.path.split(item)[0]
+            if not os.path.exists(path_to_images_dir):
+                false_dir.append(path_to_images_dir)
+    if false_dir:
+        report.create_report('files or directories do not exist in your repository', sorted(false_dir, key=str.lower))
 
 
 def yaml_validator(self):
@@ -80,8 +97,7 @@ def yaml_validator(self):
 
         required_variant_values = (['name', 'path', 'canonical'])
 
-        empty_key_check(report, data, required_keys)
-        path_to_images_dir_check(report, data)
-        variant_values_check(report, data, required_variant_values)
+        get_empty_values(report, data, required_keys)
+        get_missing_variant_keys(report, data, required_variant_values)
 
     return report
