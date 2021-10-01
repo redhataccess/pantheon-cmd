aafa3d7bb2e85c952f392ebebfa876b280f8ee86
Levi V <isaiah.sai.levi@gmail.com>
2021-10-01 02:02:55 +0200

    adds checks for path and resources

--- PantheonCMD/schema-wrapper.py
+++ PantheonCMD/schema-wrapper.py
@@ -5,6 +5,7 @@ import yaml
 from cerberus import Validator, errors
 from cerberus.errors import BasicErrorHandler
 from pcutil import is_pantheon_repo
+import glob
 
 pantheon_repo = is_pantheon_repo()
 yaml_file = pantheon_repo + 'pantheon2.yml'
@@ -14,7 +15,7 @@ class CustomErrorHandler(BasicErrorHandler):
     """Custom error messages."""
     messages = errors.BasicErrorHandler.messages.copy()
     messages[errors.REQUIRED_FIELD.code] = "key is missing"
-    messages[errors.UNKNOWN_FIELD.code] = "unknown key"
+    messages[errors.UNKNOWN_FIELD.code] = "unsupported key"
     messages[errors.NOT_NULLABLE.code] = "value can't be empty"
 
 
@@ -42,5 +43,26 @@ doc = load_doc()
 
 v.validate(doc, schema)
 
-for key in v.errors.keys():
-    print("Error in '{}': \n\t{}".format(key, ', '.join(str(item) for item in v.errors[key])))
+if v.errors:
+    for key in v.errors.keys():
+        print("Error in '{}': \n\t{}".format(key, ', '.join(str(item) for item in v.errors[key])))
+else:
+
+    path_does_not_exist = []
+    path_exists = []
+
+    for item in doc['resources']:
+        path_to_images_dir = os.path.split(item)[0]
+        if not glob.glob(path_to_images_dir):
+            path_does_not_exist.append(item)
+
+    for variant in doc['variants']:
+        if not os.path.exists(variant['path']):
+            path_does_not_exist.append(variant['path'])
+        else:
+            path_exists.append(variant['path'])
+
+    if path_does_not_exist:
+        print('\nFAIL: Your pantheon2.yml file contains the following file or directory that doesn not exist in your repository:\n')
+        for path in path_does_not_exist:
+            print('\t', path)
