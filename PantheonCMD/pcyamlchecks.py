5c6781a968a3ee4c54c3d7d1c106d037cf7efaff
Levi V <isaiah.sai.levi@gmail.com>
2021-10-01 18:21:58 +0200

    updated local validation routine

--- PantheonCMD/pcyamlchecks.py
+++ PantheonCMD/pcyamlchecks.py
@@ -1,151 +1,106 @@
 #!/usr/bin/python3
 
 import os
-import yaml
 import sys
+import yaml
+from cerberus import Validator, errors
+from cerberus.errors import BasicErrorHandler
+from pcchecks import Regex, icons_check, toc_check
+from pcvalidator import Report
 import glob
 
 
+class CustomErrorHandler(BasicErrorHandler):
+    """Custom error messages."""
+    messages = errors.BasicErrorHandler.messages.copy()
+    messages[errors.REQUIRED_FIELD.code] = "key is missing"
+    messages[errors.UNKNOWN_FIELD.code] = "unsupported key"
+    messages[errors.NOT_NULLABLE.code] = "value can't be empty"
+
+
+# test if pv2.yml is empty
 def get_yaml_size(yaml_file):
-    """Check the size of the yaml file"""
     if os.path.getsize(yaml_file) == 0:
-        return True
+        print("\nYour pantheon2.yml file is empty; exiting...")
+        sys.exit(2)
 
 
-def get_yaml_syntax_errors(yaml_file):
-    """Check the syntax of the yaml file"""
-
+def load_doc(yaml_file):
+    """Load pv2.yml and test for syntax errors."""
     with open(yaml_file, 'r') as file:
-
         try:
-            yaml.safe_load(file)
+            return yaml.safe_load(file)
         except yaml.YAMLError:
-            return True
+            print("There's a syntax error in your pantheon2.yml file. Please fix it and try again.\nTo detect an error try running yaml lint on your pantheo2.yml file.")
+            sys.exit(2)
 
 
-def get_missing_yaml_keys(yaml_file):
-    empty_keys = []
-    missing_key = []
-    missing_variant_keys = []
-    missing_variant_value = []
-    path_does_not_exist = []
-    path_exists = []
+def get_attribute_file_validation_results(attribute_file):
+    """Validate attributes file."""
+    report = Report()
 
-    required_keys = (['server', 'repository', 'variants', 'assemblies', 'modules', 'resources'])
-    required_variant_keys = (['name', 'path', 'canonical'])
+    for path in attribute_file:
+        with open(path, 'r') as file:
+            original = file.read()
+            stripped = Regex.MULTI_LINE_COMMENT.sub('', original)
+            stripped = Regex.SINGLE_LINE_COMMENT.sub('', stripped)
+            stripped = Regex.CODE_BLOCK_DASHES.sub('', stripped)
+            stripped = Regex.CODE_BLOCK_DOTS.sub('', stripped)
+            stripped = Regex.INTERNAL_IFDEF.sub('', stripped)
 
-    with open(yaml_file, 'r') as file:
-        data = yaml.safe_load(file)
-        keys = data.keys()
+            icons_check(report, stripped, path)
+            toc_check(report, stripped, path)
 
-        for key in required_keys:
-            if key not in keys:
-                missing_key.append(key)
+    return report
+
+
+def get_yaml_errors(yaml_schema, yaml_doc):
+
+    # load validator with custom error handler
+    v = Validator(yaml_schema, error_handler=CustomErrorHandler())
+
+    # validate the pv2.yml with schema
+    v.validate(yaml_doc, yaml_schema)
+
+    if v.errors:
+        print("\nFAIL: there is an error in your yaml file:")
+        for key in v.errors.keys():
+            print("\n\t'{}' {}".format(key, ', '.join(str(item) for item in v.errors[key])))
+        sys.exit(2)
+
+    else:
+
+        path_does_not_exist = []
+        path_exists = []
+
+        for item in yaml_doc['resources']:
+            path_to_images_dir = os.path.split(item)[0]
+            if not glob.glob(path_to_images_dir):
+                path_does_not_exist.append(item)
+
+        for variant in yaml_doc['variants']:
+            if not os.path.exists(variant['path']):
+                path_does_not_exist.append(variant['path'])
             else:
-                if data[key] is None:
-                    empty_keys.append(key)
-
-        if 'resources' in keys:
-            if data['resources'] is not None:
-                for item in data['resources']:
-                    path_to_images_dir = os.path.split(item)[0]
-                    if not glob.glob(path_to_images_dir):
-                        path_does_not_exist.append(item)
-
-        itemized_required_keys = []
-        existing_variant_keys = []
-        itemized_existing_variant_keys = []
-
-        if 'variants' in keys:
-            if data['variants'] is not None:
-                for variant_key in required_variant_keys:
-                    itemized_required_keys.append(variant_key)
-
-                    for variant in data['variants']:
-                        existing_variant_keys.append(variant)
-
-                        if variant[variant_key] is None:
-                            missing_variant_value.append(variant_key)
-
-                        if variant['path'] is not None:
-
-                            if not os.path.exists(variant['path']):
-                                path_does_not_exist.append(variant['path'])
-                            else:
-                                path_exists.append(variant['path'])
-
-                for item in existing_variant_keys:
-                    for i in item:
-                        itemized_existing_variant_keys.append(i)
-
-                for item in itemized_required_keys:
-                    if item not in itemized_existing_variant_keys:
-                        missing_variant_keys.append(item)
-
-    return missing_key, empty_keys, missing_variant_keys, missing_variant_value, path_does_not_exist, path_exists
-
-
-def checks(yaml_file, report):
-    missing_keys, empty_keys, missing_variant_keys, missing_variant_value, path_does_not_exist, path_exists = get_missing_yaml_keys(yaml_file)
-
-    if missing_keys:
-        report.create_report('file is missing the following keys', missing_keys)
-
-    if empty_keys:
-        report.create_report('file is missing the following values', empty_keys)
-
-
-    if missing_variant_keys:
-        report.create_report('file is missing the following keys under "variants"', missing_variant_keys)
-
-
-    if missing_variant_value:
-        report.create_report('file is missing the following keys under "variants"', missing_variant_value)
-
+                path_exists.append(variant['path'])
 
     if path_does_not_exist:
-        report.create_report('file contains the following file or directory that doesn not exist in your repository"', path_does_not_exist)
+        print('\nFAIL: Your pantheon2.yml contains the following files or directories that do not exist in your repository:\n')
+        for path in path_does_not_exist:
+            print('\t', path)
+        sys.exit(2)
+    else:
+        attribute_file_validation = get_attribute_file_validation_results(path_exists)
+        if attribute_file_validation.count != 0:
+            print("\nYour attributes file has the following errors:\n")
+            attribute_file_validation.print_report()
 
 
+def yaml_validation(pv2_yaml_file, schema):
+    """Validate pv2.yml; get path to attributes while we're at it."""
 
+    loaded_yaml = load_doc(pv2_yaml_file)
 
+    get_yaml_size(pv2_yaml_file)
 
-'''def get_missing_keys(yaml_file, report):
-    """Return missing keys."""
-    missing_keys, empty_keys, missing_variant_keys, missing_variant_value, path_does_not_exist, path_exists = get_missing_yaml_keys(yaml_file)
-
-    if missing_keys:
-        report.create_report('file is missing the following keys', missing_keys)
-
-
-def get_empty_values(yaml_file, report):
-    """Return empty keys."""
-    missing_keys, empty_keys, missing_variant_keys, missing_variant_value, path_does_not_exist, path_exists = get_missing_yaml_keys(yaml_file)
-
-    if empty_keys:
-        report.create_report('file is missing the following values', empty_keys)
-
-
-def get_missing_variant_keys(yaml_file, report):
-    """Return missing variant keys."""
-    missing_keys, empty_keys, missing_variant_keys, missing_variant_value, path_does_not_exist, path_exists = get_missing_yaml_keys(yaml_file)
-
-    if missing_variant_keys:
-        report.create_report('file is missing the following keys under "variants"', missing_variant_keys)
-
-
-def get_missing_variant_value(yaml_file, report):
-    """Return missing variant values."""
-    missing_keys, empty_keys, missing_variant_keys, missing_variant_value, path_does_not_exist, path_exists = get_missing_yaml_keys(yaml_file)
-
-    if missing_variant_value:
-        report.create_report('file is missing the following values under "variants"', missing_variant_value)
-
-
-def get_nonexistent_path(yaml_file, report):
-    """Return wrong path."""
-    missing_keys, empty_keys, missing_variant_keys, missing_variant_value, path_does_not_exist, path_exists = get_missing_yaml_keys(yaml_file)
-
-    if path_does_not_exist:
-        report.create_report('file contains the following file or directory that doesn not exist in your repository', path_does_not_exist)
-'''
+    get_yaml_errors(schema, loaded_yaml)
