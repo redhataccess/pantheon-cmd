e0878cdc293ffae80a2cda91a8b2f2d21bf06868
Levi V <isaiah.sai.levi@gmail.com>
2021-11-08 18:56:19 +0100

    script for linux installation

---
+++ linux-cmd-intallation.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Error handling
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+terminate()
+{
+    echo >&2 '
+*** terminated ***
+'
+    echo "\"$last_command\" command failed with exit code $?."
+    exit 1
+}
+
+trap 'terminate' 0
+
+set -e
+
+# clone repo
+
+# install required packages
+sudo dnf install subversion
+sudo dnf install rpm-build
+
+
+if [[ $(basename "$PWD") ]] == "pantheon-cmd"; then
+    release_number=$(grep -o -P '(?<=Release:   ).*(?=%{)' build/pantheon-cmd.spec)
+    incremented_release_number=$((release_number+1))
+else
+    echo "Navigate to the pantheon-cmd directory."
+    exit 2
+fi
+
+sh make.sh $incremented_release_number
+
+# install dependencies
+sudo gem install asciidoctor concurrent-ruby haml tilt
+
+trap : 0
+
+echo >&2 '
+*** DONE ***
+'
