af5e3a99b58f85c659a983cece3512420d4ccfaa
Levi V <isaiah.sai.levi@gmail.com>
2021-11-08 16:01:22 +0100

    mor unittests + some fixes

--- test/test_pcchecks.py
+++ test/test_pcchecks.py
@@ -169,6 +169,100 @@ xref:human-readable-label_not-present[]."""
         self.assertTrue(result, "Should return True when xref has no human readable label.")
 
 
+class TestHumanReadableLabelCheckLinks(unittest.TestCase):
+    def test_label_present(self):
+        file_contents = """= Heading
+
+[role="_abstract"]
+This is examle abstract and http://www.sample-link.com[present]."""
+
+        result = human_readable_label_check_links(file_contents)
+        self.assertIsNone(result, "Should return None when link has a human readable label.")
+
+    def test_label_not_present(self):
+        file_contents = """= Heading
+
+[role="_abstract"]
+This is examle abstract and http://www.sample-link.com[]."""
+
+        result = human_readable_label_check_links(file_contents)
+        self.assertTrue(result, "Should return True when link has no human readable label.")
+
+
+class TestHtmlMarkupCheck(unittest.TestCase):
+    def test_html_markup_present(self):
+        file_contents = """= Heading
+
+<html>markup</html>"""
+
+        result = html_markup_check(file_contents)
+        self.assertTrue(result, "Should return True when file has HTML markup.")
+
+    def test_html_markup_present(self):
+        file_contents = """= Heading
+
+<nothtml>markup<nothtml>"""
+
+        result = html_markup_check(file_contents)
+        self.assertIsNone(result, "Should return None when file has no HTML markup.")
+
+
+class TestMestingInModules(unittest.TestCase):
+    def setUp(self):
+        self.file_path = "some/path"
+
+    def test_nested_assembly_in_module(self):
+        report = Report()
+
+        file_contents = """= Heading
+
+Some sample text.
+include::assembly_some-assembly.adoc[]"""
+
+        result = nesting_in_modules_check(report, file_contents, self.file_path)
+        self.assertIn('nesting in modules. nesting', report.report)
+
+    def test_nested_module_in_module(self):
+        report = Report()
+
+        file_contents = """= Heading
+
+Some sample text.
+include::proc_some-module.adoc[]"""
+
+        result = nesting_in_modules_check(report, file_contents, self.file_path)
+        self.assertIn('nesting in modules. nesting', report.report)
+
+    def test_no_nested_in(self):
+        report = Report()
+
+        file_contents = ""
+
+        result = nesting_in_modules_check(report, file_contents, self.file_path)
+        self.assertNotIn('nesting in modules. nesting', report.report)
+
+
+
+
+class TestLvloffsetCheck(unittest.TestCase):
+    def test_lvloffset_tag_not_present(self):
+        file_contents = """= Heading
+
+include::some-include.adoc[]"""
+
+        result = lvloffset_check(file_contents)
+        self.assertIsNone(result, "Should return None when file has no :leveloffset: tag.")
+
+    def test_lvloffset_tag_present(self):
+        file_contents = """= Heading
+
+:leveloffset:
+include::some-include.adoc[]"""
+
+        result = lvloffset_check(file_contents)
+        self.assertTrue(result, "Should return True when file has no :leveloffset: tag.")
+
+
 class TestAbstractTagCheck(unittest.TestCase):
     def test_tag_present(self):
         file_contents = """"= Heading
