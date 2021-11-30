3b4356c00b051ed47fb25258a036d47f97252072
Levi V <isaiah.sai.levi@gmail.com>
2021-12-01 00:06:49 +0100

    changes module-type tag to content-type tag

--- PantheonCMD/validation/pcchecks.py
+++ PantheonCMD/validation/pcchecks.py
@@ -19,7 +19,7 @@ class Regex:
 
     INCLUDE = re.compile(r'include::.*\]\n')
     INCLUDED_CONTENT = re.compile(r'(?<=include::).*?(?=\[)')
-    MODULE_TYPE = re.compile(r':_module-type: (PROCEDURE|CONCEPT|REFERENCE)')
+    CONTENT_TYPE = re.compile(r':_content-type: (PROCEDURE|CONCEPT|REFERENCE|ASSEMBLY)')
     PREFIX_ASSEMBLIES = re.compile(r'.*\/assembly.*\.adoc')
     PREFIX_MODULES = re.compile(r'.*\/con.*\.adoc|.*\/proc.*\.adoc|.*\/ref.*\.adoc')
     # should exclude pseudo vanilla like <<some content>>
