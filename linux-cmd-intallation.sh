0363ab8627330e6aab088c183c19d7dcca7241ca
Levi V <isaiah.sai.levi@gmail.com>
2022-02-16 16:50:27 +0100

    rework of linux install script + slight changes to mac script

--- linux-cmd-intallation.sh
+++ linux-cmd-intallation.sh
@@ -15,27 +15,44 @@ trap 'terminate' 0
 
 set -e
 
-# clone repo
-
 # install required packages
-sudo dnf install subversion
-sudo dnf install rpm-build
+echo 'Installing package dependencies...'
+sudo dnf install python3 ruby subversion rpm-build
 
+echo 'Installing ruby gem dependencies...'
+sudo gem install asciidoctor concurrent-ruby haml tilt
+# do we still need pygit2?
+pip3 install pygit2
 
-if [[ $(basename "$PWD") = "pantheon-cmd" ]]; then
-    release_number=$(grep -o -P '(?<=Release:   ).*(?=%{)' build/pantheon-cmd.spec)
-    incremented_release_number=$((release_number+1))
-else
-    echo "Navigate to the pantheon-cmd directory."
-    exit 2
+echo 'Creating resources directories...'
+for dir in "PantheonCMD/haml" "PantheonCMD/locales"; do if [ ! -d "$dir" ]; then mkdir $dir; fi; done
+
+echo 'Getting HAML templates...'
+svn checkout https://github.com/redhataccess/pantheon/trunk/pantheon-bundle/src/main/resources/apps/pantheon/templates/haml/html5 PantheonCMD/haml
+
+echo 'Getting locales...'
+svn checkout https://github.com/asciidoctor/asciidoctor/trunk/data/locale PantheonCMD/locales
+rm -rf PantheonCMD/{haml,locales}/.svn
+
+echo 'Updating styling references...'
+sed -i 's/^-\ pantheonCssPath.*/-\ pantheonCssPath\ \=\ \"resources\/rhdocs.min.css\"/' PantheonCMD/haml/document.html.haml
+sed -i 's/href\=\"https\:\/\/static\.redhat\.com\/libs\/redhat\/redhat-font\/2\/webfonts\/red-hat-font\.css/href\=\"resources\/red-hat-font.css/' PantheonCMD/haml/document.html.haml
+
+echo 'Copying the source files to the local binaries directory...'
+sudo cp -r PantheonCMD /usr/local/bin
+
+# do we need to account for different shells?
+# exit code is 0 so setting +e
+echo 'Adding an alias to the current shell...'
+if [[ $SHELL = '/bin/bash' ]]; then
+    set +e && alias pcmd='/usr/bin/python3 /usr/local/bin/PantheonCMD/pcmd.py $@' && source ~/.bashrc
+elif [[ $SHELL = '/bin/tcsh' ]]; then
+    set +e && alias pcmd '/usr/bin/python3 /usr/local/bin/PantheonCMD/pcmd.py $@' && source ~/.tcshrc
+elif [[ $SHELL = '/bin/csh' ]]; then
+    set +e && alias pcmd '/usr/bin/python3 /usr/local/bin/PantheonCMD/pcmd.py $@' && source ~/.cshrc
 fi
 
-sh make.sh $incremented_release_number
-
-# install dependencies
-sudo gem install asciidoctor concurrent-ruby haml tilt
-
-sudo dnf install build/pantheon-cmd-*.noarch.rpm
+rm -rf PantheonCMD/{haml,locales}
 
 trap : 0
 
